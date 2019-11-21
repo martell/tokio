2c6bc2d26889892e1d8169c0067debca95a04f1c
Deployment Bot (from Azure Pipelines) <deploy@tokio-rs.com>
2019-11-21 02:06:38 +0000

    Deploy Tokio API documentation

---
+++ doc/tokio/runtime/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Builder","Builds Tokio Runtime with custom configuration values."],["Handle","Handle to the runtime"],["Runtime","The Tokio runtime."]],"trait":[["Park","Block the current thread."],["Unpark","Unblock a thread blocked by the associated [`Park`] instance."]]});
